c71959556ea0e187c2a70bba89a5a3cebf8fb12d
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2021-10-27 17:55:47 -0300

    first endpoint to get data

---
+++ README.md
@@ -0,0 +1,22 @@
+# portfolio-api
+
+## Try it locally
+
+* Configure env variables
+
+API_KEY=
+AUTH_DOMAIN=
+DATABASE_URL=
+PROJECT_ID=
+STORAGE_BUCKET=
+MESSAGING_SENDER_ID=
+APP_ID=?
+MEASUREMENT_ID=
+
+* Install the dependencies
+
+`npm i`
+
+* Run it
+
+`npm start`
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,60 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('my-portfolio');
+//TODO: move to a controller
+//TODO: use secrets
+
+var firebase = require("firebase/app");
+require("firebase/database");
+
+const {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} = process.env;
+
+console.log("DATABASE_URL")
+
+console.log(process.env.DATABASE_URL)
+
+var config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+firebase.initializeApp(config);
+
+router.get("/", function (req, res, next) {
+
+  console.log(config)
+
+  try {
+    const dbRef = firebase.database().ref();
+    dbRef
+      .get()
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          res.send(snapshot.val());
+        } else {
+          res.send("No data available");
+        }
+      })
+      .catch((error) => {
+        res.send(error);
+      });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 module.exports = router;
